f6bf51043f0016877ba49daf5f4a4d4abb7d34c3
Vijay_7747 <98015814+HTTPSVIJAY@users.noreply.github.com>
2023-07-10 20:49:59 +0530

    List and Tuples in python

---
+++ chapter_4listmethod.py
@@ -0,0 +1,30 @@
+# listmethod sorting 
+l1 = [1,8,7,2,21,15]
+print(l1)
+l1.sort()
+print(l1)
+
+#listmethod reverse
+l2 = [1,8,7,2,21,15]
+l2.sort()
+l2.reverse()
+print(l2)
+
+#listmethod append
+l3 = [1,8,7,2,21,15]
+l3.sort()
+l3.append(45)
+l3.append(83)
+print(l3)
+
+#listmethod insert
+l4 = [1,8,7,2,21,15]
+print(l4)
+
+#listmethod pop
+l3.pop(2)
+print(l3)
+
+#listmethod remove
+l3.remove(15)
+print(l3)
