ad539c7c118d5aea60602674aabe213005ca7388
Vijay_7747 <98015814+HTTPSVIJAY@users.noreply.github.com>
2023-07-10 20:46:45 +0530

    Variables and Datatypes in python

---
+++ chapter_2datatypes_var.py
@@ -0,0 +1,9 @@
+
+a = '''vijay'''
+b = 2133
+c = 20.25
+d = True
+print(a)
+print(b)
+print(c)
+print(d)
---
+++ chapter_2datatypes_var1.1.py
@@ -0,0 +1,16 @@
+a = '''vijay'''
+b = 2133
+c = 20.25
+d = True
+# d = None 
+B = 201
+print(a)
+print(b)
+print (B)
+print
+print(c)
+print(d)
+print (type(a))
+print (type(b))
+print (type(c))
+print (type(d))
